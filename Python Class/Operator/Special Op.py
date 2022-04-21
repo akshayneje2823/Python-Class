9d83e2040ad3060acfdc6b80551dca5c688d2cc2
Akshay Neje <akshayneje2823@gmail.com>
2022-04-21 18:52:52 +0530

    ok

---
+++ Python Class/Operator/Special Op.py
@@ -0,0 +1,16 @@
+# Membership Operator ==> Address
+
+frameWorks = ["Bootstrap","React","Django"]
+print("JavaScript" in(frameWorks))
+print("React" in(frameWorks))       
+print("DJango" in(frameWorks))      #False Check Spell
+print("Django" not in(frameWorks))  #False
+
+
+# Comparism Opertor
+
+something = 10
+print( 10 is(something))      # It will check that 10 is present in something or not
+print( 10 is not(something))
+
+
