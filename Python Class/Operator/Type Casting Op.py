c8b0ab74b12f1ebacd6fa623270a155e1a3b63f8
Akshay Neje <akshayneje2823@gmail.com>
2022-04-21 18:27:20 +0530

    Python class

---
+++ Python Class/Operator/Type Casting Op.py
@@ -0,0 +1,26 @@
+# Using this op we can conavrt one type to another type.
+
+
+a = "10"
+print(int(a))
+
+b = 10
+print(float(b))
+
+c = 10
+print(bin(c))
+
+d = 26
+print(hex(d))
+
+e = 256
+print(oct(e))
+
+f = 23
+print(bool(f))
+
+g = ""            # bool ==> It has to be contain some value otherwise it will say False
+print(bool(g))  
+
+h = 10.6
+print(complex(h))
